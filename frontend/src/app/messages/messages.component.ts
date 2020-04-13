46f9ee0116f38cff801c0a041cdbaa5aa684170e
alislahiprahim <alislahiprahim99@gmail.com>
2020-04-13 06:14:02 +0200

    ---

--- frontend/src/app/messages/messages.component.ts
+++ frontend/src/app/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { patientService } from '../services/patient.service';
 
@@ -19,7 +19,7 @@ export class MessagesComponent implements OnInit {
   TP: any
   D_Name: string
   imageURL: string
-  show_chat=false
+  show_chat = false
   show = false
   DId: any;
   patient_chat: any = [];
@@ -52,26 +52,27 @@ export class MessagesComponent implements OnInit {
 
   gettreatmentPlan() {
 
-    this.mypatientService.getTreatmentPlan().subscribe((resp: any) => {
-      console.log(resp)
-      if (resp == 'authentication failed') {
-        window.alert('login please')
-        this.show = false
-      } else {
-        this.show = true
-        this.TP = resp
-        this.D_Name = this.TP.d_username
-        this.imageURL = this.TP.d_avatar
-        this.DId = this.TP.result.DId
-        this.patient_chat = resp.patient_chat
+    if (!this.TP) {
+      this.mypatientService.getTreatmentPlan().subscribe((resp: any) => {
+        console.log(resp)
+        if (resp == 'authentication failed') {
+          window.alert('login please')
+          this.show = false
+        } else {
+          this.show = true
+          this.TP = resp
+          this.D_Name = this.TP.d_username
+          this.imageURL = this.TP.d_avatar
+          this.DId = this.TP.result.DId
+          this.patient_chat = resp.patient_chat
 
-      }
-    })
-
+        }
+      })
+    }
 
   }
 
-  
+
   toggle() {
     this.show_chat = !this.show_chat;
   }
